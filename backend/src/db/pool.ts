2ecbf8bf8c03d56057727fd2575d999d1449a59d
Loic Fuentes <fuentes.loic@gmail.com>
2025-02-27 19:37:49 -0500

    fix-40: mysql env

--- backend/src/db/pool.ts
+++ backend/src/db/pool.ts
@@ -1,17 +1,10 @@
 import mysql from "mysql2/promise";
 
-console.log("Database pool configuration:", {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-});
-
 export const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_NAME || "test",
+  host: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
