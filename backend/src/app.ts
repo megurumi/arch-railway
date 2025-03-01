ca03b0d50dd4a741362fcedaa9be09679af5a6c7
Loic Fuentes <fuentes.loic@gmail.com>
2025-03-01 16:28:32 -0500

    fix-54: refacto api start

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -20,14 +20,12 @@ registerUserRoutes(app);
 
 const start = async () => {
   try {
-    await app.listen({ port });
-    console.log(`Server listening at http://localhost:${port}`);
-    app.log.info(`Server listening at http://localhost:${port}`);
+    await app.listen({ port, host: "0.0.0.0" });
+    app.log.info(`Server listening on port ${port}`);
   } catch (err) {
-    console.error("Error starting Fastify app", { err });
     app.log.error(err);
     process.exit(1);
   }
 };
 
-export { start };
+start();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,4 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { start } from "./app";
-
-start();
+import "./app.ts";
