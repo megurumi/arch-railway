572dadab45740e813675b9caa02e309069eb8719
Loic Fuentes <fuentes.loic@gmail.com>
2025-03-01 16:10:58 -0500

    fix-52: move api start

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,3 +17,42 @@ app.register(fastifyCors, {
 // Register routes
 registerHealthRoutes(app);
 registerUserRoutes(app);
+
+const start = async () => {
+  console.log(`Fastify app has been registered for port ${port}`);
+  console.log(app);
+
+  try {
+    app.ready((err) => {
+      console.log("Fastify app is ready", {
+        app,
+        port,
+        err,
+      });
+
+      if (err) {
+        console.error("App is not ready! Errors detected.", { err });
+        app.log.error(err);
+        process.exit(1);
+      }
+
+      console.log("App listener starting...");
+      app.listen({ port }, (err, address) => {
+        if (err) {
+          console.error("App is not listening! Errors detected.", { err });
+          app.log.error(err);
+          process.exit(1);
+        }
+
+        console.log(`Server listening at ${address}`);
+        app.log.info(`Server listening at ${address}`);
+      });
+    });
+  } catch (err) {
+    console.error("Error starting Fastify app", { err });
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+
+start();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,43 +1,4 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { app, port } from "./app";
-
-const start = async () => {
-  console.log(`Fastify app has been register for port ${port}`);
-  console.log(app);
-
-  try {
-    app.ready((err) => {
-      console.log("Fastify app is ready", {
-        app,
-        port,
-        err,
-      });
-
-      if (err) {
-        console.error("App is not ready! Errors detected.", { err });
-        app.log.error(err);
-        process.exit(1);
-      }
-
-      console.log("App listenner starting...");
-      app.listen({ port }, (err, address) => {
-        if (err) {
-          console.error("App is not listening! Errors detected.", { err });
-          app.log.error(err);
-          process.exit(1);
-        }
-
-        console.log(`Server listening at ${address}`);
-        app.log.info(`Server listening at ${address}`);
-      });
-    });
-  } catch (err) {
-    console.error("Error starting Fastify app", { err });
-    app.log.error(err);
-    process.exit(1);
-  }
-};
-
-start();
+import "./app";
