db2cc2dce6443449b0ef1bbf58f16c3630e5aea3
Loic Fuentes <fuentes.loic@gmail.com>
2025-03-01 15:52:26 -0500

    fix-49: backend log

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,8 @@ import { DEFAULT_BACKEND_PORT } from "./constants";
 export const app = fastify({ logger: true });
 export const port = parseInt(process.env.BACKEND_PORT || DEFAULT_BACKEND_PORT);
 
+console.log("Initializing Fastify app...");
+
 // Enable CORS
 app.register(fastifyCors, {
   origin: "*",
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,16 +4,28 @@ dotenv.config();
 import { app, port } from "./app";
 
 app.ready((err) => {
+  console.log("Fastify app is ready", {
+    app,
+    port,
+    err,
+  });
+
   if (err) {
+    console.error("App is not ready! Errors detected.", { err });
     app.log.error(err);
     process.exit(1);
   }
 
+  console.log("No errors detected. Starting server...");
+
   app.listen({ port }, (err, address) => {
     if (err) {
+      console.error("App is not listening! Errors detected.", { err });
       app.log.error(err);
       process.exit(1);
     }
+
+    console.log(`Server listening at ${address}`);
     app.log.info(`Server listening at ${address}`);
   });
 });
