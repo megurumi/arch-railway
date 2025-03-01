d65eb11d3ca21ab2401ab916025ee4f37cd7d132
Loic Fuentes <fuentes.loic@gmail.com>
2025-03-01 17:26:31 -0500

    fix-61: valid flow

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -25,7 +25,6 @@ const start = async () => {
       port,
       host: "0.0.0.0",
     });
-    app.log.info(`Server listening on port ${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
