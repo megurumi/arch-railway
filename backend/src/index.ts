09bd865d8da9922194d25660bcc61ba1c1cd2d5f
Loic Fuentes <fuentes.loic@gmail.com>
2025-03-01 16:58:01 -0500

    fix-57: refacto api start

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,25 +1,4 @@
-import fastify from "fastify";
-
 import dotenv from "dotenv";
-
 dotenv.config();
 
-const app = fastify({ logger: true });
-
-app.ready((err) => {
-  if (err) {
-    app.log.error(err);
-
-    process.exit(1);
-  }
-
-  app.listen({ port: 8080 }, (err, address) => {
-    if (err) {
-      app.log.error(err);
-
-      process.exit(1);
-    }
-
-    app.log.info(`Server listening at ${address}`);
-  });
-});
+import "./app.ts";
