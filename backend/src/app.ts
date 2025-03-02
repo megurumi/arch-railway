80ad76f9da5693fa6d9395b8ac26efcbe83acc62
Loic Fuentes <fuentes.loic@gmail.com>
2025-03-01 19:11:48 -0500

    feat-66: add stepper logs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,23 +9,23 @@ const port = parseInt(process.env.BACKEND_PORT || DEFAULT_BACKEND_PORT);
 
 const start = async () => {
   const app = fastify({ logger: true });
-  app.log.info("Initializing Fastify app...");
+  app.log.info("1/5 - Initializing Fastify app...");
 
   try {
     app.register(fastifyCors, {
       origin: "*",
     });
-    app.log.info("Cors has been registered");
+    app.log.info("2/5 - Cors has been registered");
 
     registerHealthRoutes(app);
     registerUserRoutes(app);
-    app.log.info("Routes has been registered");
+    app.log.info("3/5 - Routes has been registered");
 
     await app.ready();
-    app.log.info("Server app is ready");
+    app.log.info("4/5 - Server app is ready");
 
     await app.listen({ port, host: "0.0.0.0" });
-    app.log.info(`Server listening on port ${port}`);
+    app.log.info(`5/5 - Server listening on port ${port}`);
   } catch (err) {
     app.log.error(err);
     process.exit(1);
