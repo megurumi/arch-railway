c2ee1f32ecfacc132cf2d93793eb07e95ba681d5
Loic Fuentes <fuentes.loic@gmail.com>
2025-03-01 19:05:26 -0500

    feat-65: add log on fast instance for better logs

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,29 @@
 import fastify from "fastify";
 import fastifyCors from "@fastify/cors";
+
 import { registerHealthRoutes } from "./routes/health";
 import { registerUserRoutes } from "./routes/users";
 import { DEFAULT_BACKEND_PORT } from "./constants";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const port = parseInt(process.env.BACKEND_PORT || DEFAULT_BACKEND_PORT);
 
-const app = fastify({ logger: true });
-app.log.info("Initializing Fastify app...");
-
-// Enable CORS
-app.register(fastifyCors, {
-  origin: "*",
-});
-
-// Register routes
-registerHealthRoutes(app);
-registerUserRoutes(app);
-
 const start = async () => {
+  const app = fastify({ logger: true });
+  app.log.info("Initializing Fastify app...");
+
   try {
+    app.register(fastifyCors, {
+      origin: "*",
+    });
+    app.log.info("Cors has been registered");
+
+    registerHealthRoutes(app);
+    registerUserRoutes(app);
+    app.log.info("Routes has been registered");
+
     await app.ready();
+    app.log.info("Server app is ready");
+
     await app.listen({ port, host: "0.0.0.0" });
     app.log.info(`Server listening on port ${port}`);
   } catch (err) {
